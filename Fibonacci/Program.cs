a07fcef37348f3458c338af3f4338b47150c6265
StarryNight00 <cisa.matias@gmail.com>
2019-03-29 12:42:43 +0000

    adicionado exc Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número");
+            string aux = Console.ReadLine();
+            int n = Convert.ToInt32(aux);
+            int p = n;
+
+            Fibonacci(p);
+
+            Console.WriteLine(n + " está na posição " + p + " na sequência de Fibonacci.");
+        }
+
+        static int Fibonacci(int n)
+        {
+            n = 1;
+            return n;
+        }
+    }
+}
