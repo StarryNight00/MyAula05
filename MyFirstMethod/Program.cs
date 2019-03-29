0bbaee3a6242b319caf156d4b4e83b428fb0c99f
StarryNight00 <cisa.matias@gmail.com>
2019-03-29 11:46:31 +0000

    Ex1 - adicionado um Overload de CountToN

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -5,7 +5,7 @@ namespace MyFirstMethod
     class Program
     {
         /// <summary>
-        /// Main of the program, all of the main components of the program are here.
+        /// Main of the program, all of the main components to run the program are here.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
@@ -20,6 +20,21 @@ namespace MyFirstMethod
             CountToN(nCount2);
 
             CountTo10();
+
+            Console.WriteLine("\n");
+            CountToN(3, 76);
+        }
+
+        /// <summary>
+        /// For cycle to print in console all numbers between 1 and 10.
+        /// Uses the CountToN method with an int 10 within ( ). 
+        /// </summary>
+        static void CountTo10()
+        {
+            CountToN(10);
+
+            //for (int i = 1; i <= 10; i++)
+            //Console.WriteLine(i);
         }
 
         /// <summary>
@@ -33,16 +48,17 @@ namespace MyFirstMethod
                 Console.WriteLine(i);
         }
 
-        /// <summary>
-        /// For cycle to print in console all numbers between 1 and 10.
-        /// Uses the CountToN method with an int 10 within ( ). 
-        /// </summary>
-        static void CountTo10()
-        {
-            CountToN(10);
 
-            //for (int i = 1; i <= 10; i++)
-                //Console.WriteLine(i);
+        /// <summary>
+        /// For cycle to print in console all numbers between n1 and n2.
+        /// </summary>
+        /// <param name="n1"> Variable Type int. Sets the start number of the For cycle.</param>
+        /// <param name="n2"> Variable Type int. Sets the end number of the For cycle.</param>
+        static void CountToN(int n1, int n2)
+        {
+            for (int i = n1; i <= n2; i++)
+                Console.WriteLine(i);
         }
+
     }
 }
