56117fdb5818cf590adc139866cde4a20736c76c
StarryNight00 <cisa.matias@gmail.com>
2019-03-29 09:43:08 +0000

    Ex1 - troca ordem main e metodo

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,16 +4,16 @@ namespace MyFirstMethod
 {
     class Program
     {
-        static void CountTo10()
-        {
-            for (int i = 1; i <= 10; i++)
-                Console.WriteLine(i);
-        }
-
         static void Main(string[] args)
         {
             CountTo10();
             CountTo10();
+        }
+
+        static void CountTo10()
+        {
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine(i);
         }
     }
 }
