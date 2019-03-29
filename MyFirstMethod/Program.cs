38e9c7ffdd04289ed6e87bee3f165d968599898d
StarryNight00 <cisa.matias@gmail.com>
2019-03-29 10:43:33 +0000

    Ex1 - CountToN e CountTo10

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -6,14 +6,29 @@ namespace MyFirstMethod
     {
         static void Main(string[] args)
         {
-            CountTo10();
+            //1a vez
+            string aux1 = Console.ReadLine();
+            int nCount1 = Convert.ToInt32(aux1);
+            CountToN(nCount1);
+
+            string aux2 = Console.ReadLine();
+            int nCount2 = Convert.ToInt32(aux2);
+            CountToN(nCount2);
+
             CountTo10();
         }
 
+        static void CountToN(int n)
+        {
+            for (int i = 1; i <= n; i++)
+                Console.WriteLine(i);
+        }
         static void CountTo10()
         {
-            for (int i = 1; i <= 10; i++)
-                Console.WriteLine(i);
+            CountToN(10);
+
+            //for (int i = 1; i <= 10; i++)
+                //Console.WriteLine(i);
         }
     }
 }
